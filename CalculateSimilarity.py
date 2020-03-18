67878a86c6dfe84e815fb91439a0e93bda5e80e3
NVD919 <49793442+NVD919@users.noreply.github.com>
2020-03-18 11:58:56 -0500

    Update CalculateSimilarity.py

--- CalculateSimilarity.py
+++ CalculateSimilarity.py
@@ -3,6 +3,8 @@
 Created on Wed Mar 18 11:43:02 2020
 
 @author: Robot Hands | www.github.com/NVD919 | NVD#0729
+
+I've included a second method for querying the model in a commented out section below that doesn't require Flask.
 """
 
 from gensim.models import FastText
@@ -13,7 +15,8 @@ model = FastText.load('Embedding.model')
 
 comparison = Flask(__name__)
 
-@watcher.route('/', methods = ['POST'])
+# You need to have Flask installed to be able to run this API.  Run it and post your sentences as JSON packets in Watchman or however you like to do that.
+@comparison.route('/', methods = ['POST'])
 def distance():
     json = request.json
     print(json)
@@ -27,3 +30,15 @@ def distance():
 
 if __name__ == '__main__':
     comparison.run()
+    
+"""
+#Use this one if you just want to play around with it by feeding your own sentences in your IDE / Terminal
+
+sentence1 = "I'm in quarantined at home."
+sentence2 = "I can't go anywhere because of the virus."
+stopword = stopwords.words("english")
+sentence1 = [w for w in sentence1 if w not in stopword]
+sentence2 = [w for w in sentence2 if w not in stopword]
+distance = model.wv.wmdistance(sentence1, sentence2)
+print("Numerical representation of the difference between sentences: {}".format(distance))
+"""
